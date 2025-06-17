6488200d4e38343435cf119a23072db4f1416882
Vanessacat524 <Vanessa.Kaywood@yahoo.com>
2025-06-16 18:21:19 -0700

    initial commit

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-import pandas as pd
-import plotly.express as px
+import streamlit as st
 
-# Load dataset
-df = pd.read_csv('/Users/anits/tripleten_test/vehicles_us.csv')
-
-# Example histograms
-fig = px.histogram(df, x='price')
-fig.show()
-
-# Example scatterplot
-fig = px.scatter(df, x='odometer', y='price', color='condition')
-fig.show()
+st.header("Cointoss example")
