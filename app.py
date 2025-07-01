7b393e0abad3c33844f99db845ca757c5dfff55b
Vanessacat524 <Vanessa.Kaywood@yahoo.com>
2025-07-01 16:31:44 -0700

    add changes

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+import pandas as pd
 import streamlit as st
+import plotly.express as px
 
-st.header("Cointoss example")
+
+#read the csv file here
+data=pd.read_csv('vehicles_us.csv')
+
+
+#use the st.checkbox
+
+st.header('Vehicles')
+
+number_of_trials = st.slider('Car Names', 1, 1000, 10)
+start_button = st.button('Run')
+
+
+
+#st.write(px.histogram(...))
+fig = px.histogram(data,x='price')
+fig.show()
+#st.write(px.scattr(....))
+fig = px.scatter(data,x='odometer',y='price',color='condition')
+fig.show()
